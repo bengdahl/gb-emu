8e84e30b175f09c9a8e959f4f0f735eb1c53b9e2
Ben <bengdahl341@gmail.com>
2021-05-23 18:03:44 +0000

    switch to `Application` instead of `Sandbox`

--- gb/src/main.rs
+++ gb/src/main.rs
@@ -1,19 +1,36 @@
-use iced::{Element, Sandbox};
+use iced::{Application, Element};
 
-struct App {}
+struct App {
+    _gameboy: gb_core::gameboy::Gameboy<gb_core::gameboy::models::DMG>,
+}
 
-impl Sandbox for App {
+impl Application for App {
+    type Executor = iced::executor::Default;
+    type Flags = ();
     type Message = ();
 
-    fn new() -> Self {
-        App {}
+    fn new(_flags: ()) -> (Self, iced::Command<()>) {
+        static BLARGG_TEST_ROM: &[u8] = include_bytes!("cpu_instrs.gb");
+
+        let app = App {
+            _gameboy: gb_core::gameboy::Gameboy::new(Vec::from(BLARGG_TEST_ROM)).unwrap(),
+        };
+
+        let cmd = iced::Command::none();
+        (app, cmd)
     }
 
     fn title(&self) -> String {
         String::from("Hello world")
     }
 
-    fn update(&mut self, message: Self::Message) {}
+    fn update(
+        &mut self,
+        _message: Self::Message,
+        _clip: &mut iced::Clipboard,
+    ) -> iced::Command<()> {
+        iced::Command::none()
+    }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
         iced::Text::new("Hello, world!").into()
@@ -21,5 +38,5 @@ impl Sandbox for App {
 }
 
 fn main() {
-    App::run(iced::Settings::default());
+    App::run(iced::Settings::default()).unwrap();
 }
