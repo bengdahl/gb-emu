b36e43bf1e3d23a9be99cd5e6b3082792a0fec88
Ben Engdahl <bengdahl341@gmail.com>
2021-05-22 23:47:28 -0400

    Graphical hello world

--- gb/src/main.rs
+++ gb/src/main.rs
@@ -1,3 +1,25 @@
+use iced::{Element, Sandbox};
+
+struct App {}
+
+impl Sandbox for App {
+    type Message = ();
+
+    fn new() -> Self {
+        App {}
+    }
+
+    fn title(&self) -> String {
+        String::from("Hello world")
+    }
+
+    fn update(&mut self, message: Self::Message) {}
+
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        iced::Text::new("Hello, world!").into()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::run(iced::Settings::default());
 }
